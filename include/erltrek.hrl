f9a390370ec9b74eef94175735997dcc5d2c6df9
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-22 09:49:59 +0900

    Add erltrek.hrl
    * Macros for defining game-wide constants

---
+++ include/erltrek.hrl
@@ -0,0 +1,12 @@
+%% dimensions of quadrant in sectors
+-define(NSECTS, 10).
+%% dimension of galaxy in quadrants
+-define(NQUADS, 8).
+%% maximum klingons per quadrant
+-define(MAXKLQUAD, 9).
+%% maximum number of starbases in galaxy
+-define(MAXBASES, 9).
+%% max number of concurrently pending events
+-define(MAXEVENTS, 25).
+%% maximum concurrent distress calls
+-define(MAXDISTR, 5).
