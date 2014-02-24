c47f3aae69d6ced353d54abdc68ec941820d35a5
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-24 21:13:24 +0900

    Update erltrek_setup.erl
    * Remove randsector/1
    
    * Add randsector_quad/3
    
    * Modify test/1 to use randsector_quad/3
    
    * Use per-sector state atoms of s_* form, e.g, s_empty, s_star, etc.

--- src/erltrek_setup.erl
+++ src/erltrek_setup.erl
@@ -3,23 +3,8 @@
 -include("erltrek.hrl").
 
 -export([test/0,
-        randsector/1,
         randsector_quad/3]).
 
-%% return empty {qx, qy, sx, sy} in the galaxy
-randsector(G) ->
-    randsector(G, {}, false).
-
-randsector(_, C, true) ->
-    C;
-randsector(G, _, false) ->
-    QX = tinymt32:uniform(?NQUADS) - 1,
-    QY = tinymt32:uniform(?NQUADS) - 1,
-    SX = tinymt32:uniform(?NSECTS) - 1,
-    SY = tinymt32:uniform(?NSECTS) - 1,
-    randsector(G, {QX, QY, SX, SY}, 
-        array:get(?GALAXYCOORD(QX, QY, SX, SY), G) =:= empty).
-    
 %% return empty {qx, qy, sx, sy} in the specified quadrant
 randsector_quad(G, QX, QY) ->
     randsector_quad(G, QX, QY, {}, false).
@@ -30,20 +15,22 @@ randsector_quad(G, QX, QY, _, false) ->
     SX = tinymt32:uniform(?NSECTS) - 1,
     SY = tinymt32:uniform(?NSECTS) - 1,
     randsector_quad(G, QX, QY, {SX, SY}, 
-        array:get(?GALAXYCOORD(QX, QY, SX, SY), G) =:= empty).
+        array:get(?GALAXYCOORD(QX, QY, SX, SY), G) =:= s_empty).
 
 %% test
 test() -> 
     G = array:new(?NSECTS * ?NSECTS * ?NQUADS * ?NQUADS, 
-                  {default, empty}),
+                  {default, s_empty}),
     test(G, [], 100).
 
 test(G, L, 0) ->
     {G, L};
 test(G, L, N) ->
-    C = randsector(G),
-    {QX, QY, SX, SY} = C,
-    G1 = array:set(?GALAXYCOORD(QX, QY, SX, SY), star, G),
-    L2 = lists:append(L, [C]),
+    QX = tinymt32:uniform(?NQUADS) - 1,
+    QY = tinymt32:uniform(?NQUADS) - 1,
+    C = randsector_quad(G, QX, QY),
+    {SX, SY} = C,
+    G1 = array:set(?GALAXYCOORD(QX, QY, SX, SY), s_star, G),
+    L2 = lists:append(L, [{QX, QY, SX, SY}]),
     test(G1, L2, N - 1).
 
