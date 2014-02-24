fcb39fd5dbfa1234fe683f8d067f182e756e20c3
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-25 08:45:01 +0900

    Update erltrek.hrl
    * Add ?QUADCOORD and ?SECTCOORD

--- include/erltrek.hrl
+++ include/erltrek.hrl
@@ -96,6 +96,10 @@
 %% convert coordinates to Galaxy array position
 -define(GALAXYCOORD(QX, QY, SX, SY), 
     ((((QX * ?NQUADS) + QY) * (?NSECTS * ?NSECTS)) + ((SX*?NSECTS) + SY))).
+%% convert quadrant coordinates to Quad array position
+-define(QUADCOORD(QX, QY), (QX * ?NQUADS) + QY)).
+%% convert sector coordinates to Sect array position
+-define(SECTCOORD(SX, SY), (SX * ?NSECTS) + SY)).
 
 %% vim: set ts=4 sw=4 sts=4 et :
 %% emacs: -*- mode:erlang; tab-width:4; indent-tabs-mode:nil;  -*-
