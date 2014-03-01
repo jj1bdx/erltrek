9a2faf530b4a5868fb68ff41badaa7aebcaefc36
Kenji Rikitake <kenji.rikitake@acm.org>
2014-03-01 20:14:27 +0900

    Update erltrek_event.erl
    * Remove unnecessary code in timer_tasks/2

--- src/erltrek_event.erl
+++ src/erltrek_event.erl
@@ -98,21 +98,9 @@
             dict(), dict(), dict(), dict(), dict(), array(), dict()}}.
 
 timer_tasks(Tick, GameState) ->
-    GameState2 = enterprise_command(Tick, GameState),
-    %QC = #quadxy{x = tinymt32:uniform(?NQUADS) - 1,
-    %             y = tinymt32:uniform(?NQUADS) - 1},
-    %{SECT2, DKS2} = erltrek_setup:setup_sector(QC, DS, DI, DB, DH, DKQ),
-    % put enterprise in the current quadrant
-    %SC = erltrek_setup:rand_sect(SECT2),
-    %SECT3 = array:set(erltrek_setup:sectxy_index(SC), s_enterprise, SECT2), 
-    %SHIP2 = SHIP#enterprise_status{quadxy = QC, sectxy = SC},
-    % displaying the status
-    %erltrek_scan:srscan(Tick, SHIP2, SECT3, DI, DKQ),
-    %erltrek_scan:lrscan(SHIP2, DS, DI, DB, DKQ),
-
+    % Enterprise action
+    NewGameState = enterprise_command(Tick, GameState),
     % Set new game state
-
-    NewGameState = GameState2,
     NewGameState.
 
 %% Do commands for Enterprise
