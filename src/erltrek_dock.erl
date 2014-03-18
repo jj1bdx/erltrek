0c7b533d5fe49489e99ed59d974b405a09cda249
Kenji Rikitake <kenji.rikitake@acm.org>
2014-03-18 13:49:28 +0900

    Add erltrek_dock.erl
    * Add dock/undock functions

---
+++ src/erltrek_dock.erl
@@ -0,0 +1,151 @@
+%%% --------------------------------------------------------------------
+%%% Erltrek ("this software") is covered under the BSD 3-clause
+%%% license.
+%%%
+%%% This product includes software developed by the University of
+%%% California, Berkeley and its contributors.
+%%%
+%%% Copyright (c) 2014 Kenji Rikitake. All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%%
+%%% * Redistributions of source code must retain the above copyright
+%%%   notice, this list of conditions and the following disclaimer.
+%%%
+%%% * Redistributions in binary form must reproduce the above
+%%%   copyright notice, this list of conditions and the following
+%%%   disclaimer in the documentation and/or other materials provided
+%%%   with the distribution.
+%%%
+%%% * Neither the name of Kenji Rikitake, k2r.org, nor the names of
+%%%   its contributors may be used to endorse or promote products
+%%%   derived from this software without specific prior written
+%%%   permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+%%% CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+%%% INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+%%% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+%%% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+%%% BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+%%% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+%%% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+%%% ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+%%% TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+%%% THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+%%% SUCH DAMAGE.
+%%%
+%%% This software incorporates portions of the BSD Star Trek source
+%%% code, distributed under the following license:
+%%%
+%%% Copyright (c) 1980, 1993
+%%%      The Regents of the University of California.
+%%%      All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%% 1. Redistributions of source code must retain the above copyright
+%%%    notice, this list of conditions and the following disclaimer.
+%%% 2. Redistributions in binary form must reproduce the above
+%%%    copyright notice, this list of conditions and the following
+%%%    disclaimer in the documentation and/or other materials provided
+%%%    with the distribution.
+%%% 3. All advertising materials mentioning features or use of this
+%%%    software must display the following acknowledgement:
+%%%      This product includes software developed by the University of
+%%%      California, Berkeley and its contributors.
+%%% 4. Neither the name of the University nor the names of its
+%%%    contributors may be used to endorse or promote products derived
+%%%    from this software without specific prior written permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS
+%%% IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+%%% REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+%%% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+%%% OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+%%% EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%
+%%% [End of LICENSE]
+%%% --------------------------------------------------------------------
+
+-module(erltrek_dock).
+
+-include("erltrek.hrl").
+
+-export([dock/1,
+        undock/1
+        ]).
+
+%% Docking a starbase
+
+-spec dock(game_state()) -> game_state().
+
+dock(GameState) ->
+    {_Tick, SHIP, _NK, _DS, _DI, _DB, _DH, _DKQ, _SECT, _DKS} = GameState,
+    case SHIP#enterprise_status.docked of
+        true ->
+            erltrek_event:notify({dock, already_docked}),
+            GameState;
+        false ->
+            try_docking(GameState)
+    end.
+        
+%% Undocking from a starbase
+
+-spec undock(game_state()) -> game_state().
+
+undock(GameState) ->
+    {Tick, SHIP, NK, DS, DI, DB, DH, DKQ, SECT, DKS} = GameState,
+    case SHIP#enterprise_status.docked of
+        false ->
+            erltrek_event:notify({undock, not_docked}),
+            GameState;
+        true ->
+            SHIP2 = SHIP#enterprise_status{docked = false},
+            erltrek_event:notify({undock, undock_complete}),
+            {Tick, SHIP2, NK, DS, DI, DB, DH, DKQ, SECT, DKS}
+    end.
+
+%% Try docking a starbase
+
+-spec try_docking(game_state()) -> game_state().
+
+try_docking(GameState) ->
+    {Tick, SHIP, NK, DS, DI, DB, DH, DKQ, SECT, DKS} = GameState,
+    QC = SHIP#enterprise_status.quadxy,
+    SC = SHIP#enterprise_status.sectxy,
+    SX = SC#sectxy.x,
+    SY = SC#sectxy.y,
+    SHIP2 = case dict:is_key(QC, DB) of
+        % Starbase exists in the quadrant
+        true ->
+            [TB] = dict:fetch(QC, DB),
+            BC = TB#base_info.xy,
+            BX = BC#sectxy.x,
+            BY = BC#sectxy.y,
+            % If the starbase is at an adjacent sector,
+            % Enterprise can be docked
+            case abs(SX - BX) =< 1 andalso abs(SY - BY) =< 1 of
+                true ->
+                    erltrek_event:notify({dock, dock_complete}),
+                    SHIP#enterprise_status{docked = true};
+                false ->
+                    erltrek_event:notify({dock, base_not_adjacent}),
+                    SHIP
+            end;
+        false ->
+            erltrek_event:nofify({dock, base_not_in_quadrant}),
+            SHIP
+    end,
+    {Tick, SHIP2, NK, DS, DI, DB, DH, DKQ, SECT, DKS}.
+
