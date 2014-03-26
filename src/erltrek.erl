17faa7532f577d3321c41b28f22c2b0db49b61c0
Kenji Rikitake <kenji.rikitake@acm.org>
2014-03-26 15:59:17 +0900

    erltrek.erl: add typespec

--- src/erltrek.erl
+++ src/erltrek.erl
@@ -39,8 +39,10 @@
 
 -export([start/0, stop/0]).
 
+-spec start() -> ok | {error, term()}.
 start() ->
     application:start(erltrek).
 
+-spec stop() -> ok | {error, term()}.
 stop() ->
     application:stop(erltrek).
