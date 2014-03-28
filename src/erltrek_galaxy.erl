6b32199033eba50f3c44fd25e04938e4814c074f
Kenji Rikitake <kenji.rikitake@acm.org>
2014-03-28 19:00:09 +0900

    More comment added to erltrek_galaxy:move_ships/2

--- src/erltrek_galaxy.erl
+++ src/erltrek_galaxy.erl
@@ -433,8 +433,11 @@ move_ships(Delta, #state{ ships=Ships }=State0) ->
                             pos=#galaxy{x = GX, y = GY},
                             speed=Speed, course=Course }=Data,
                    Acc) ->
-                      % Note: Delta unit in Seconds
-                      % 0 =< Speed * Delta < 1.0
+                      % Note from @jj1bdx:
+                      % Delta unit in Seconds
+                      % abs(Speed * Delta) < 1.0
+                      % Otherwise skipping sectors may occur
+                      % @kaos: am I correct?
                       Dist = Speed * Delta,
                       DX = Dist * -math:cos(Course),
                       DY = Dist * math:sin(Course),
